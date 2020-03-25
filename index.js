b4a810c78a3121c346a20389cfa26f3501e74039
Precious-amie <61975077+Precious-amie@users.noreply.github.com>
2020-03-25 01:16:12 +0000

    added a index.js file.

---
+++ index.js
@@ -0,0 +1,14 @@
+let myName = "Onyemenem Amarachi"
+let myCoursesArray =["Html", "CSS", "Javascript", "UI/UX"]
+
+    console. log("My name is" +  myName);
+    console. log(myCoursesArray);
+
+
+const data =[];
+for (let i=1; i<=200; i++) {
+    if (i%2===0) {
+     data.push(i); 
+    };
+} 
+console.log(data); 
